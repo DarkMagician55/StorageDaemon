ac8cb1cdefc242262796c22af88ecb50ac32a0fa
guo <darkmagicianguo@gmail.com>
2016-09-06 18:38:32 +0800

    init

---
+++ src/main/java/com/dut/service/StorageDaemon.java
@@ -0,0 +1,18 @@
+package com.dut.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by guo on 9/6/16.
+ */
+public class StorageDaemon {
+    private static Logger logger = LoggerFactory.getLogger(StorageDaemon.class);
+
+    public static void main(String[] args)
+    {
+        logger.info("test");
+//        new StorageDaemon().activate();
+    }
+
+}
